ea5ec834e1625a8b854fa28d06554507916d0e8f
João Henrique Lopes Linux <joaohenriquelopesdiv@gmail.com>
2025-02-03 22:57:36 -0300

    Feat: User Service

---
+++ src/main/java/com/desafio_picpay/services/UserService.java
@@ -0,0 +1,46 @@
+package com.desafio_picpay.services;
+
+import com.desafio_picpay.domain.user.User;
+import com.desafio_picpay.domain.user.UserType;
+import com.desafio_picpay.dto.UserDTO;
+import com.desafio_picpay.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    public void validateTransaction(User sender, BigDecimal amount) throws Exception{
+        if(sender.getUserType() == UserType.MERCHANT){
+            throw new Exception("Autorização negada!");
+        }
+
+        if(sender.getBalance().compareTo(amount) < 0){
+            throw new Exception("Saldo insuficiente!");
+        }
+    }
+
+    public User findUserById(Long id) throws Exception{
+        return this.repository.findUserById(id).orElseThrow(() -> new Exception("Usuário não encontrado!"));
+    }
+
+    public void saveUser(User user){
+        this.repository.save(user);
+    }
+
+    public User createUser(UserDTO data){
+        User newUser = new User(data);
+        this.saveUser(newUser);
+        return newUser;
+    }
+
+    public List<User> getAllUsers(){
+        return this.repository.findAll();
+    }
+}
