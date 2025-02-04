e53854b347787d9c447bd1f452fdc429f3cc550d
João Henrique Lopes Linux <joaohenriquelopesdiv@gmail.com>
2025-02-04 16:54:38 -0300

    Feat: Transaction Controller

---
+++ src/main/java/com/desafio_picpay/controllers/TransactionController.java
@@ -0,0 +1,23 @@
+package com.desafio_picpay.controllers;
+
+import com.desafio_picpay.domain.transaction.Transaction;
+import com.desafio_picpay.dto.TransactionDTO;
+import com.desafio_picpay.services.TransactionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/transactions")
+public class TransactionController {
+
+    @Autowired
+    private TransactionService transactionService;
+
+    @PostMapping
+    private ResponseEntity<Transaction> createTransaction(@RequestBody TransactionDTO transaction) throws Exception{
+        Transaction newTransaction = this.transactionService.createTransaction(transaction);
+        return new ResponseEntity<>(newTransaction, HttpStatus.OK);
+    }
+}
