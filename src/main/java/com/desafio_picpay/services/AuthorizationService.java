c3db9e817fa8497a79f1a4214389ada9a0119983
João Henrique - Linux <joaohenriquelopesdiv@gmail.com>
2025-03-19 14:48:41 -0300

    Chore: Spring Security configurations

---
+++ src/main/java/com/desafio_picpay/services/AuthorizationService.java
@@ -0,0 +1,18 @@
+package com.desafio_picpay.services;
+
+import com.desafio_picpay.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class AuthorizationService implements UserDetailsService {
+
+    @Autowired
+    UserRepository repository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return repository.findUserByEmail(username);
+    }
+}
