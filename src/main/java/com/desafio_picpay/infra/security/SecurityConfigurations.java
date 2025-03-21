98a5e3f65968f51ae9a89e44a7cc37a3194fb3f3
João Henrique - Linux <joaohenriquelopesdiv@gmail.com>
2025-03-21 16:48:28 -0300

    Feat: Route authorizations

--- src/main/java/com/desafio_picpay/infra/security/SecurityConfigurations.java
+++ src/main/java/com/desafio_picpay/infra/security/SecurityConfigurations.java
@@ -2,6 +2,9 @@ package com.desafio_picpay.infra.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -16,7 +19,25 @@ public class SecurityConfigurations {
         return httpSecurity
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(authorize -> authorize
+                        //.requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
+                        //.requestMatchers(HttpMethod.POST, "/users").permitAll()
+                        //.requestMatchers(HttpMethod.POST, "/transactions").hasRole("commun")
+                        //.requestMatchers(HttpMethod.GET, "/transactions").hasRole("merchant")
+
+                        .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/users").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/users").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/transactions").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/transactions").permitAll()
+
+                )
                 .build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception{
+        return authenticationConfiguration.getAuthenticationManager();
     }
 
 }
