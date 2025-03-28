00edfa4422acc33fadf80ad57dcc783acd216fc7
João Henrique - Linux <joaohenriquelopesdiv@gmail.com>
2025-03-28 13:58:44 -0300

    Fix: Authentication Register Route

--- src/main/java/com/desafio_picpay/controllers/AuthenticationController.java
+++ src/main/java/com/desafio_picpay/controllers/AuthenticationController.java
@@ -1,8 +1,14 @@
 package com.desafio_picpay.controllers;
 
+import com.desafio_picpay.domain.user.User;
 import com.desafio_picpay.dto.AuthenticationDTO;
+import com.desafio_picpay.dto.UserDTO;
+import com.desafio_picpay.repositories.UserRepository;
+import com.desafio_picpay.services.UserService;
+import com.desafio_picpay.utils.ValidarCPF;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -21,6 +27,12 @@ public class AuthenticationController {
     @Autowired
     private AuthenticationManager authenticationManager;
 
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private UserRepository repository;
+
     @PostMapping("/login")
     public ResponseEntity login(@RequestBody @Validated AuthenticationDTO data){
         var usernamePassword = new UsernamePasswordAuthenticationToken(data.email(), data.password());
@@ -29,8 +41,17 @@ public class AuthenticationController {
         return ResponseEntity.ok().build();
     }
 
-    @Bean
-    public PasswordEncoder passwordEncoder(){
-        return new BCryptPasswordEncoder();
+
+    @PostMapping("/register")
+    public ResponseEntity register(@RequestBody @Validated UserDTO data){
+        if(this.repository.findUserByEmail(data.email()) != null) return ResponseEntity.badRequest().build();
+
+        String encryptedPassword = new BCryptPasswordEncoder().encode(data.password());
+        User newUser = new User(data.firstName(), data.lastName(), data.document(), data.email(), encryptedPassword, data.balance(), data.userType());
+
+        this.repository.save(newUser);
+
+        return ResponseEntity.ok().build();
     }
+
 }
