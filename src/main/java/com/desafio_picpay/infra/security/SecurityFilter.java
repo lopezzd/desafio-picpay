eb213b84c9c166258dbdf78106b70e8930ee1aaf
João Henrique - Linux <joaohenriquelopesdiv@gmail.com>
2025-04-09 21:48:21 -0300

    Feat: Security Filter

---
+++ src/main/java/com/desafio_picpay/infra/security/SecurityFilter.java
@@ -0,0 +1,48 @@
+package com.desafio_picpay.infra.security;
+
+import com.desafio_picpay.repositories.UserRepository;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class SecurityFilter extends OncePerRequestFilter {
+
+    @Autowired
+    TokenService tokenService;
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        var token = this.recoverToken(request);
+
+        if(token != null){
+            var email = tokenService.validateToken(token);
+            UserDetails user = userRepository.findUserByEmail(email);
+
+            var authentication = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+    private String recoverToken(HttpServletRequest request){
+        var authHearder = request.getHeader("Authorization");
+
+        if(authHearder == null) return null;
+
+        return authHearder.replace("Bearer", "");
+    }
+}
